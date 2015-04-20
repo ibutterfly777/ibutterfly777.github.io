19816d03abe0f7bb0db1efd206095508defb10e2
Inna <prudkaya.inna@gmail.com>
2015-04-20 23:36:08 +0300

    CBS

--- CBS/test/navigator.js
+++ CBS/test/navigator.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+(function($){
 	if (navigator.geolocation) {
 	    navigator.geolocation.getCurrentPosition(function(position){
 	    var latitude = position.coords.latitude;
