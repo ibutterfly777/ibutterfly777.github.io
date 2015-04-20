a2e9ef2c08447ba750348b80a2928bbdbbfcee23
Inna <prudkaya.inna@gmail.com>
2015-04-20 23:52:58 +0300

    Update navigator.js

--- CBS/test/navigator.js
+++ CBS/test/navigator.js
@@ -1,4 +1,4 @@
-function( ){
+function navigator.geolocation( ){
 	if (navigator.geolocation) {
 	    navigator.geolocation.getCurrentPosition(function(position){
 	    var latitude = position.coords.latitude;
